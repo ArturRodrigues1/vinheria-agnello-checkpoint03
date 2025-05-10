145f38ac7a89f2804349dc41b3f0ce0d7091b6d3
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-05-10 17:40:43 -0300

    Função para validar entrada de dados

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 let nome1, tipo1, safra1, quant1;
 let nome2, tipo2, safra2, quant2;
 let nome3, tipo3, safra3, quant3;
@@ -9,3 +8,11 @@ let nome7, tipo7, safra7, quant7;
 let nome8, tipo8, safra8, quant8;
 let nome9, tipo9, safra9, quant9;
 let nome10, tipo10, safra10, quant10;
+
+function validarEntrada(mensagem) {
+    let entrada;
+    do {
+    entrada = prompt(mensagem);
+    } while (entrada === null || entrada.trim() === "");
+    return entrada;
+}
